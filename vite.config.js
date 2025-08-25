60ee24f7a9ece041f8555d10a90d7cb53c63645d
Akram Alyuosfi <Akram Alyuosfi@DESKTOP-AKRAM-A>
2025-08-26 01:19:42 +0300

    Build Vite and update vite.config.js for production

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,4 @@
-import {
-    defineConfig
-} from 'vite';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from "@tailwindcss/vite";
 
@@ -15,4 +13,10 @@ export default defineConfig({
     server: {
         cors: true,
     },
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+        emptyOutDir: true,
+    },
+    base: process.env.APP_URL ? process.env.APP_URL + '/' : '/',
 });
